e80917df948523f3bc41d1e7fe369699f7fc638d
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2017-01-02 15:09:49 +0200

    Made: produser - add HttpServer and routing web - start streaming from form.

--- produser/src/main/scala/com/aleksandrbogomolov/Main.scala
+++ produser/src/main/scala/com/aleksandrbogomolov/Main.scala
@@ -1,23 +1,29 @@
 package com.aleksandrbogomolov
 
 import com.aleksandrbogomolov.stream.{AbstractStream, FilterStream}
-import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.HttpServerRequest
+import io.vertx.core.{AbstractVerticle, Handler}
 import org.apache.spark.streaming.dstream.DStream
 import twitter4j.Status
 
 class Main extends AbstractVerticle {
 
-  val filters: Array[String] = Array("#scala", "#java", "#groovy", "#kotlin")
-
   var stream: AbstractStream = _
 
   override def start(): Unit = {
-    stream = new FilterStream(filters)
-    vertx.deployVerticle(Publisher)
-    val statuses: DStream[Status] = stream.startStream()
-    statuses.foreachRDD(_.foreachPartition(_.foreach(Publisher.publish)))
-    stream.configuration.streamingContext.start()
-    stream.configuration.streamingContext.awaitTermination()
+    vertx.createHttpServer.requestHandler(new Handler[HttpServerRequest] {
+      override def handle(event: HttpServerRequest): Unit = {
+        if (event.path().contains("start")) {
+          val filters: Array[String] = event.getParam("filter").split(",")
+          stream = new FilterStream(filters)
+          vertx.deployVerticle(Publisher)
+          val statuses: DStream[Status] = stream.startStream()
+          statuses.foreachRDD(_.foreachPartition(_.foreach(Publisher.publish)))
+          stream.configuration.streamingContext.start()
+          stream.configuration.streamingContext.awaitTermination()
+        }
+      }
+    }).listen(8888)
   }
 
   override def stop(): Unit = stream.stopStream()
